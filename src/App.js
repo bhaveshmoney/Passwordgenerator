fa22ad5e45e4a115e97250dec33937af878617d1
Bhavesh Singh <bhaveshsingh75@gmail.com>
2021-03-12 18:29:22 +0530

    Implemet the fuctionality in PasswordGenertaor App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,58 @@
-
+import React, { useState } from 'react';
 import './App.css';
+import { number,UpperCaseLetters,LowerCaseLetter,specialCharacter } from "./characters";
 
 function App() {
+
+     const [password, setPassword] = useState('');
+     const [passwordLength,setPasswordLength] =useState('20');
+     const [includeUppercase,setincludeUppercase]=useState(false);
+     const [includeLowercase,setincludeLowercase]=useState(false);
+     const [includeNumber,setincludeNumber]=useState(false);
+     const [includeSymbol,setincludeSymbol]=useState(false);
+   
+     const handledgeneratepassword= (e) =>{
+       
+      let characterList='';
+       if (includeUppercase) {
+        characterList= characterList + UpperCaseLetters;
+       }
+       if (includeLowercase) {
+        characterList= characterList + LowerCaseLetter;
+       }
+       if (includeNumber) {
+        characterList= characterList + number;
+       }
+       if (includeSymbol) {
+        characterList= characterList + specialCharacter;
+       }
+       setPassword(createPassword(characterList));
+     }
+     const createPassword = (characterList) =>{
+       let password ='';
+
+       const CharacterListLength =characterList.length;
+       for (let i = 0; i < passwordLength; i++) {
+         const characterIndex= Math.round(Math.random() * CharacterListLength)  ;
+          
+         password =password +characterList.charAt(characterIndex)
+         
+       }
+       return password
+     }
+     const copyToClipboard =() =>{
+       const newTextarea = document.createElement('textarea');
+       newTextarea.innerText =password
+       document.body.appendChild(newTextarea);
+       newTextarea.select()
+       document.execCommand('copy')
+       newTextarea.remove()
+     }
+
+     const handleCopyPassword= (e) =>{
+       copyToClipboard()
+     }
+
   return (
     <div className="App">
       <div className="container">
@@ -10,32 +61,59 @@ function App() {
              Password Generator
             </h2>
             <div className="generator_password">
-              <h3>Password</h3>
-              <button className="copy_btn">
+              <h3>{password}</h3>
+              <button onClick={handleCopyPassword} className="copy_btn">
               <i class="fas fa-clipboard"></i>
               </button>
             </div>
             <div className="form-group">
                <label htmlFor="password-length">Password Length</label>
-               <input type="number" id="password-length" name="password-length" max="8" min="20"/> 
+               <input
+               defaultValue={passwordLength}
+               onChange={ (e) =>setPasswordLength(e.target.value)}
+                type="number"
+                 id="password-length" 
+                 name="password-length"
+                  max="8"
+                   min="20"/> 
             </div>
             <div className="form-group">
                <label htmlFor="uppercase-letter"> Include Uppercase Letter</label>
-               <input type="checkbox" id="uppercase-letter" name="uppercase-letter" /> 
+               <input
+               checked={includeUppercase}
+               onChange={ (e) =>setincludeUppercase(e.target.checked)}
+                type="checkbox"
+                 id="uppercase-letter"
+                  name="uppercase-letter" /> 
             </div>
             <div className="form-group">
                <label htmlFor="lowercase-letter">Include lowercase Letter</label>
-               <input type="checkbox" id="lowercase-letter" name="lowercase-letter" /> 
+               <input 
+               checked={includeLowercase}
+               onChange={ (e) =>setincludeLowercase(e.target.checked)}
+               type="checkbox" 
+               id="lowercase-letter"
+                name="lowercase-letter" /> 
             </div>
             <div className="form-group">
                <label htmlFor="number">Include Number</label>
-               <input type="checkbox" id="number" name="number" /> 
+               <input
+               checked={includeNumber}
+               onChange={ (e)=>setincludeNumber(e.target.checked)} 
+               type="checkbox" 
+               id="number" 
+               name="number" /> 
             </div>
             <div className="form-group">
                <label htmlFor="symbol">Include Symbol</label>
-               <input type="checkbox" id="symbol" name="symbol" /> 
+               <input 
+               checked={includeSymbol}
+               onChange={ (e)=>setincludeSymbol(e.target.checked)}
+               type="checkbox" 
+               id="symbol" 
+               name="symbol" /> 
             </div>
-            <button className="generate_btn">Generate Button</button>
+            <button onClick={handledgeneratepassword} className="generate_btn" >Generate Button</button>
           </div>
       </div> 
     </div>
---
+++ src/characters.jsx
@@ -0,0 +1,4 @@
+export const number='0123456789';
+export const UpperCaseLetters='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+export const LowerCaseLetter='abcdefghijklmopqrstuvwxyz';
+export const specialCharacter='!@#$%^&*()-+=+_{[]}.,?/|<:;""';
